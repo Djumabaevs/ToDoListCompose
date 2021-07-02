b70f38f4eb23904ca5a77be2c4621dc871590cef
Djumabaevs <djumabaevb@gmail.com>
2021-07-02 10:42:10 +0600

    db mapper interface

---
+++ app/src/main/java/com/example/todolistcompose/data/database/dbmapper/DbMapper.kt
@@ -0,0 +1,29 @@
+package com.example.todolistcompose.data.database.dbmapper
+
+interface DbMapper {
+
+    // NoteDbModel -> NoteModel
+
+    fun mapNotes(
+        noteDbModels: List<NoteDbModel>,
+        colorDbModels: Map<Long, ColorDbModel>
+    ): List<NoteModel>
+
+    fun mapNote(noteDbModel: NoteDbModel, colorDbModel: ColorDbModel): NoteModel
+
+    // ColorDbModel -> ColorModel
+
+    fun mapColors(colorDbModels: List<ColorDbModel>): List<ColorModel>
+
+    fun mapColor(colorDbModel: ColorDbModel): ColorModel
+
+    // NoteModel -> NoteDbModel
+
+    fun mapDbNote(note: NoteModel): NoteDbModel
+
+    // ColorModel -> ColorDbModel
+
+    fun mapDbColors(colors: List<ColorModel>): List<ColorDbModel>
+
+    fun mapDbColor(color: ColorModel): ColorDbModel
+}
---
+++ app/src/main/java/com/example/todolistcompose/data/database/dbmapper/DbMapperImpl.kt
@@ -0,0 +1,4 @@
+package com.example.todolistcompose.data.database.dbmapper
+
+class DbMapperImpl {
+}
