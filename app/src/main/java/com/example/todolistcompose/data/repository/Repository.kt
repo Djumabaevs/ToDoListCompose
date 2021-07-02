994e0a025d765c6b09b35fc2a7bbd9667cc702f9
Djumabaevs <djumabaevb@gmail.com>
2021-07-02 10:36:43 +0600

    Repository implementation

---
+++ app/src/main/java/com/example/todolistcompose/data/repository/Repository.kt
@@ -0,0 +1,35 @@
+package com.example.todolistcompose.data.repository
+
+import androidx.lifecycle.LiveData
+
+
+interface Repository {
+
+    // notes
+
+    fun getAllNotesNotInTrash(): LiveData<List<NoteModel>>
+
+    fun getAllNotesInTrash(): LiveData<List<NoteModel>>
+
+    fun getNote(id: Long): LiveData<NoteModel>
+
+    fun insertNote(note: NoteModel)
+
+    fun deleteNote(id: Long)
+
+    fun deleteNotes(noteIds: List<Long>)
+
+    fun moveNoteToTrash(noteId: Long)
+
+    fun restoreNotesFromTrash(noteIds: List<Long>)
+
+    // colors
+
+    fun getAllColors(): LiveData<List<ColorModel>>
+
+    fun getAllColorsSync(): List<ColorModel>
+
+    fun getColor(id: Long): LiveData<ColorModel>
+
+    fun getColorSync(id: Long): ColorModel
+}
