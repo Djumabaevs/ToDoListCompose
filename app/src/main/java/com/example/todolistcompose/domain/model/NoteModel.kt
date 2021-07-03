716a60d5dfb1ab9785f6cd882b6956eb227acf7b
Djumabaevs <djumabaevb@gmail.com>
2021-07-03 12:21:38 +0600

    note model data class

--- app/src/main/java/com/example/todolistcompose/domain/model/NoteModel.kt
+++ app/src/main/java/com/example/todolistcompose/domain/model/NoteModel.kt
@@ -1,4 +1,9 @@
 package com.example.todolistcompose.domain.model
 
-class NoteModel {
-}
+data class NoteModel(
+    val id: Long = NEW_NOTE_ID, // This value is used for new notes
+    val title: String = "",
+    val content: String = "",
+    val isCheckedOff: Boolean? = null, // null represents that the note can't be checked off
+    val color: ColorModel = ColorModel.DEFAULT
+)
