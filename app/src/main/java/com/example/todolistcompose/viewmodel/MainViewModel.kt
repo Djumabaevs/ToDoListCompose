d3479c04f4d076f8c613383a76451dcf519514aa
Djumabaevs <djumabaevb@gmail.com>
2021-07-05 00:18:33 +0600

    main view model

--- app/src/main/java/com/example/todolistcompose/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/todolistcompose/viewmodel/MainViewModel.kt
@@ -1,4 +1,103 @@
 package com.example.todolistcompose.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todolistcompose.data.repository.Repository
+import com.example.todolistcompose.domain.model.ColorModel
+import com.example.todolistcompose.domain.model.NoteModel
+import com.example.todolistcompose.routing.JetNotesRouter
+import com.example.todolistcompose.routing.Screen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(private val repository: Repository) : ViewModel() {
+
+    val notesNotInTrash: LiveData<List<NoteModel>> by lazy {
+        repository.getAllNotesNotInTrash()
+    }
+
+    private var _noteEntry = MutableLiveData(NoteModel())
+    val noteEntry: LiveData<NoteModel> = _noteEntry
+
+    val colors: LiveData<List<ColorModel>> by lazy {
+        repository.getAllColors()
+    }
+
+    val notesInTrash by lazy { repository.getAllNotesInTrash() }
+
+    private var _selectedNotes = MutableLiveData<List<NoteModel>>(listOf())
+    val selectedNotes: LiveData<List<NoteModel>> = _selectedNotes
+
+    fun onCreateNewNoteClick() {
+        _noteEntry.value = NoteModel()
+        JetNotesRouter.navigateTo(Screen.SaveNote)
+    }
+
+    fun onNoteClick(note: NoteModel) {
+        _noteEntry.value = note
+        JetNotesRouter.navigateTo(Screen.SaveNote)
+    }
+
+    fun onNoteCheckedChange(note: NoteModel) {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.insertNote(note)
+        }
+    }
+
+    fun onNoteSelected(note: NoteModel) {
+        _selectedNotes.value = _selectedNotes.value!!.toMutableList().apply {
+            if (contains(note)) {
+                remove(note)
+            } else {
+                add(note)
+            }
+        }
+    }
+
+    fun restoreNotes(notes: List<NoteModel>) {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.restoreNotesFromTrash(notes.map { it.id })
+            withContext(Dispatchers.Main) {
+                _selectedNotes.value = listOf()
+            }
+        }
+    }
+
+    fun permanentlyDeleteNotes(notes: List<NoteModel>) {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.deleteNotes(notes.map { it.id })
+            withContext(Dispatchers.Main) {
+                _selectedNotes.value = listOf()
+            }
+        }
+    }
+
+    fun onNoteEntryChange(note: NoteModel) {
+        _noteEntry.value = note
+    }
+
+    fun saveNote(note: NoteModel) {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.insertNote(note)
+
+            withContext(Dispatchers.Main) {
+                JetNotesRouter.navigateTo(Screen.Notes)
+
+                _noteEntry.value = NoteModel()
+            }
+        }
+    }
+
+    fun moveNoteToTrash(note: NoteModel) {
+        viewModelScope.launch(Dispatchers.Default) {
+            repository.moveNoteToTrash(note.id)
+
+            withContext(Dispatchers.Main) {
+                JetNotesRouter.navigateTo(Screen.Notes)
+            }
+        }
+    }
 }
