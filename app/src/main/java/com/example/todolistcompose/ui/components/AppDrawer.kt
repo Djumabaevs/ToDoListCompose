dc2de32de19057c0ccd132885607dec944de42e2
Djumabaevs <djumabaevb@gmail.com>
2021-07-05 00:11:20 +0600

    composable app drawer

--- app/src/main/java/com/example/todolistcompose/ui/components/AppDrawer.kt
+++ app/src/main/java/com/example/todolistcompose/ui/components/AppDrawer.kt
@@ -1,10 +1,50 @@
 package com.example.todolistcompose.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todolistcompose.routing.Screen
 import com.example.todolistcompose.theme.JetNotesTheme
 
+
+@Composable
+fun AppDrawer(
+    currentScreen: Screen,
+    closeDrawerAction: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        AppDrawerHeader()
+
+        Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
+
+        ScreenNavigationButton(
+            icon = Icons.Filled.Home,
+            label = "Notes",
+            isSelected = currentScreen == Screen.Notes,
+            onClick = {
+                JetNotesRouter.navigateTo(Screen.Notes)
+                closeDrawerAction()
+            }
+        )
+        ScreenNavigationButton(
+            icon = Icons.Filled.Delete,
+            label = "Trash",
+            isSelected = currentScreen == Screen.Trash,
+            onClick = {
+                JetNotesRouter.navigateTo(Screen.Trash)
+                closeDrawerAction()
+            }
+        )
+        LightDarkThemeItem()
+    }
+}
 
 @Preview
 @Composable
