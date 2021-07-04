7a05177f1cec28688638924e17cb62e4d3f503f1
Djumabaevs <djumabaevb@gmail.com>
2021-07-04 10:33:35 +0600

    add light theme or dark theme

--- app/src/main/java/com/example/todolistcompose/theme/Theme.kt
+++ app/src/main/java/com/example/todolistcompose/theme/Theme.kt
@@ -2,10 +2,26 @@ package com.example.todolistcompose.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+
+private val LightThemeColors = lightColors(
+    primary = rwGreen,
+    primaryVariant = rwGreenDark,
+    secondary = rwGreen
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Color(0xFF00A055),
+    primaryVariant = Color(0xFF00F884),
+    secondary = rwRed,
+    onPrimary = Color.White,
+)
 
 @Composable
 fun JetNotesTheme(content: @Composable () -> Unit) {
