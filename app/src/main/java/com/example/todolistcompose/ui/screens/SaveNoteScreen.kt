e0522244e5826f96dff86e4dbc2ed2ad778ec318
Djumabaevs <djumabaevb@gmail.com>
2021-07-06 00:23:39 +0600

    composable picked color

--- app/src/main/java/com/example/todolistcompose/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/example/todolistcompose/ui/screens/SaveNoteScreen.kt
@@ -22,6 +22,29 @@ import com.example.todolistcompose.util.fromHex
 
 
 
+@Composable
+private fun PickedColor(color: ColorModel) {
+    Row(
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = "Picked color",
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        NoteColor(
+            color = Color.fromHex(color.hex),
+            size = 40.dp,
+            border = 1.dp,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+
 
 @Composable
 private fun ColorPicker(
