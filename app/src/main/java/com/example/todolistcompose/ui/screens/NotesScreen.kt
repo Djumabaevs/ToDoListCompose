4310e3427a702030626db0ed7aadbde1161722eb
Djumabaevs <djumabaevb@gmail.com>
2021-07-06 00:18:58 +0600

    notes composable

--- app/src/main/java/com/example/todolistcompose/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/example/todolistcompose/ui/screens/NotesScreen.kt
@@ -1,11 +1,92 @@
 package com.example.todolistcompose.ui.screens
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todolistcompose.domain.model.NoteModel
+import com.example.todolistcompose.routing.Screen
+import com.example.todolistcompose.ui.components.AppDrawer
+import com.example.todolistcompose.ui.components.Note
+import com.example.todolistcompose.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
 
+
+@ExperimentalMaterialApi
+@Composable
+fun NotesScreen(viewModel: MainViewModel) {
+
+    val notes: List<NoteModel> by viewModel
+        .notesNotInTrash
+        .observeAsState(listOf())
+
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "JetNotes",
+                        color = MaterialTheme.colors.onPrimary
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        coroutineScope.launch { scaffoldState.drawerState.open() }
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.List,
+                            contentDescription = "Drawer Button"
+                        )
+                    }
+                }
+            )
+        },
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            AppDrawer(
+                currentScreen = Screen.Notes,
+                closeDrawerAction = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                }
+            )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { viewModel.onCreateNewNoteClick() },
+                contentColor = MaterialTheme.colors.background,
+                content = {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Add Note Button"
+                    )
+                }
+            )
+        },
+        content = {
+            if (notes.isNotEmpty()) {
+                NotesList(
+                    notes = notes,
+                    onNoteCheckedChange = {
+                        viewModel.onNoteCheckedChange(it)
+                    },
+                    onNoteClick = { viewModel.onNoteClick(it) }
+                )
+            }
+        }
+    )
+}
 
 @ExperimentalMaterialApi
 @Composable
