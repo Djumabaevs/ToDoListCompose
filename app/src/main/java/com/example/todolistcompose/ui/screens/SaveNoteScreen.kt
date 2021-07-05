b2fff2de7ff9dcfab9d65b5b70dded9e611c0de5
Djumabaevs <djumabaevb@gmail.com>
2021-07-06 00:22:34 +0600

    composable color item

--- app/src/main/java/com/example/todolistcompose/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/example/todolistcompose/ui/screens/SaveNoteScreen.kt
@@ -1,11 +1,52 @@
 package com.example.todolistcompose.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todolistcompose.domain.model.ColorModel
 import com.example.todolistcompose.domain.model.NoteModel
+import com.example.todolistcompose.ui.components.NoteColor
+import com.example.todolistcompose.util.fromHex
 
 
-
+@Composable
+fun ColorItem(
+    color: ColorModel,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = {
+                    onColorSelect(color)
+                }
+            )
+    ) {
+        NoteColor(
+            modifier = Modifier.padding(10.dp),
+            color = Color.fromHex(color.hex),
+            size = 80.dp,
+            border = 2.dp
+        )
+        Text(
+            text = color.name,
+            fontSize = 22.sp,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .align(Alignment.CenterVertically)
+        )
+    }
+}
 
 @Preview
 @Composable
