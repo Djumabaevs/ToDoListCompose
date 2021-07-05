8c29362588d2880955c608dd29d88e91ce76bd10
Djumabaevs <djumabaevb@gmail.com>
2021-07-06 00:17:39 +0600

    notes list

--- app/src/main/java/com/example/todolistcompose/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/example/todolistcompose/ui/screens/NotesScreen.kt
@@ -1,9 +1,31 @@
 package com.example.todolistcompose.ui.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todolistcompose.domain.model.NoteModel
+
+
+@ExperimentalMaterialApi
+@Composable
+private fun NotesList(
+    notes: List<NoteModel>,
+    onNoteCheckedChange: (NoteModel) -> Unit,
+    onNoteClick: (NoteModel) -> Unit
+) {
+    LazyColumn {
+        items(count = notes.size) { noteIndex ->
+            val note = notes[noteIndex]
+            Note(
+                note = note,
+                onNoteClick = onNoteClick,
+                onNoteCheckedChange = onNoteCheckedChange,
+                isSelected = false
+            )
+        }
+    }
+}
 
 
 @ExperimentalMaterialApi
