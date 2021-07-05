df3d237b2e063baccdea3e05c9a464bb38eed1a2
Djumabaevs <djumabaevb@gmail.com>
2021-07-06 00:24:11 +0600

    composable note check option

--- app/src/main/java/com/example/todolistcompose/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/example/todolistcompose/ui/screens/SaveNoteScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,6 +22,28 @@ import com.example.todolistcompose.ui.components.NoteColor
 import com.example.todolistcompose.util.fromHex
 
 
+
+@Composable
+private fun NoteCheckOption(
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = "Can note be checked off?",
+            modifier = Modifier.weight(1f)
+        )
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
 
 @Composable
 private fun PickedColor(color: ColorModel) {
