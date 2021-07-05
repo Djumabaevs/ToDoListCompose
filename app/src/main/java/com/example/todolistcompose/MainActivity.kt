b987fc8c3aa442c52118801f3e4dff8a8c2103ad
Djumabaevs <djumabaevb@gmail.com>
2021-07-06 00:15:21 +0600

    main activity refactor

--- app/src/main/java/com/example/todolistcompose/MainActivity.kt
+++ app/src/main/java/com/example/todolistcompose/MainActivity.kt
@@ -3,36 +3,50 @@ package com.example.todolistcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolistcompose.routing.JetNotesRouter
+import com.example.todolistcompose.routing.Screen
+import com.example.todolistcompose.theme.JetNotesTheme
 import com.example.todolistcompose.ui.theme.ToDoListComposeTheme
+import com.example.todolistcompose.viewmodel.MainViewModel
+import com.example.todolistcompose.viewmodel.MainViewModelFactory
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels(factoryProducer = {
+        MainViewModelFactory(
+            this,
+            (application as ToDoApplication).dependencyInjector.repository
+        )
+    })
+
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            ToDoListComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            JetNotesTheme {
+                MainActivityScreen(viewModel = viewModel) // here
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToDoListComposeTheme {
-        Greeting("Android")
+@ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel) {
+    Surface {
+        when (JetNotesRouter.currentScreen) {
+            is Screen.Notes -> NotesScreen(viewModel)
+            is Screen.SaveNote -> SaveNoteScreen(viewModel)
+            is Screen.Trash -> TrashScreen(viewModel)
+        }
     }
 }
